377389e23dbad065ea56436533f12268a9e18b3c
Yunus Şimşek <yunuss060@gmail.com>
2023-12-31 12:59:37 +0300

    Add project files.

---
+++ eBelge_DataSyncHub/Definitions/Glb_Enum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBelge_DataSyncHub.Definitions
+{
+    internal class Glb_Enum
+    {
+    }
+}
---
+++ eBelge_DataSyncHub/Model/Projects/ELedgerDeclaration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBelge_DataSyncHub.Model.Projects
+{
+    internal class ELedgerDeclaration
+    {
+    }
+}
